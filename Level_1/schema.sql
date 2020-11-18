5243c0c9aea06e8259af352f7e14a5388e8babf0
MonicaKang <mokaex@gmail.com>
2020-11-17 22:46:41 -0800

    update

---
+++ Level_1/schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE review_id_table;
+DROP TABLE products;
+DROP TABLE customers;
+
+CREATE TABLE review_id_table (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  review_date DATE -- this should be in the formate yyyy-mm-dd
+);
+
+-- This table will contain only unique values
+CREATE TABLE products (
+  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+  product_title TEXT
+);
+
+-- Customer table for first data set
+CREATE TABLE customers (
+  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+  customer_count INT
+);
+
+
+SELECT * FROM review_id_table;
+
+SELECT * FROM products;
+
+SELECT * FROM customers;
